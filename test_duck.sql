2d11d61e8547119419ecb1c52c96e32cf8e45e02
Jales Bussinguer <63973509+JalesBussinguer@users.noreply.github.com>
2024-07-30 23:30:56 -0300

    updates on main query

--- test_duck.sql
+++ test_duck.sql
@@ -1,4 +1,10 @@
 LOAD spatial;
 
-CREATE TABLE liquor_sales AS SELECT *
-FROM 'C:\Users\jales\OneDrive\Documentos\GitHub\liquor-distribution-problem\data\iowa_liquor_sales.csv';
+CREATE TABLE liquor_sales AS SELECT * FROM 'C:\Users\jales\OneDrive\Documentos\GitHub\liquor-distribution-problem\data\iowa_liquor_sales.csv';
+
+SHOW liquor_sales;
+
+describe liquor_sales;
+
+SELECT "Store Location" from liquor_sales;
+
