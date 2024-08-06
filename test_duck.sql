ba5dd7c6c91208e96d4fa726794d5f1a54a74bcb
Jales Bussinguer <63973509+JalesBussinguer@users.noreply.github.com>
2024-08-05 23:02:43 -0300

    Updated query to transform a column on a geometry type

--- test_duck.sql
+++ test_duck.sql
@@ -4,76 +4,56 @@ CREATE TABLE liquor_sales AS SELECT * FROM 'data\iowa_liquor_sales.csv';
 
 SELECT * FROM liquor_sales LIMIT 10;
 
+SELECT "Store Location" FROM liquor_sales LIMIT 10;
+
 describe liquor_sales;
 
-FROM liquor_sales SELECT "Store Location";
-
--- This line uses regex to match the pattern of the coordinates in the column
-
---FROM liquor_sales SELECT regexp_extract("Store Location", '\(.*\)', 0) AS coordinates;
-
--- This excerpt uses a function that slice the string and locate the target substring
--- In this case, we want the coordinates within the complete string
-
+CREATE TABLE points_geom AS
 SELECT
-        "Store Location",
-        SUBSTRING(
-            "Store Location",
-            POSITION('(' IN "Store Location") + 1,
-            POSITION(')' IN "Store Location") - POSITION('(' IN "Store Location") - 1
-        ) AS coordinates
-    FROM liquor_sales;
-
-ALTER TABLE liquor_sales ADD COLUMN coordinates STRING;
-
-
--- Step 1: Add New Columns to the liquor_sales Table
-ALTER TABLE liquor_sales
-ADD COLUMN X NUMERIC;
-
-ALTER TABLE liquor_sales
-ADD COLUMN Y NUMERIC;
-
--- Step 2: Extract, Convert, and Update the liquor_sales Table
-WITH coordinates_extracted AS (
+    *,
+    CASE
+        WHEN x IS NOT NULL AND y IS NOT NULL THEN ST_Point(x, y)
+        ELSE NULL
+    END AS geom
+FROM (
     SELECT
-        "Store Location",
-        SUBSTRING(
-            "Store Location",
-            POSITION('(' IN "Store Location") + 1,
-            POSITION(')' IN "Store Location") - POSITION('(' IN "Store Location") - 1
-        ) AS coordinates
-    FROM liquor_sales
-),
-
-x_y_extracted AS (
-    SELECT
-        "Store Location",
-        coordinates,
-        SUBSTRING(coordinates, 1, POSITION(', ' IN coordinates) - 1) AS X,
-        SUBSTRING(coordinates, POSITION(', ' IN coordinates) + 2) AS Y
-    FROM coordinates_extracted
-),
-
-x_y_converted AS (
-    SELECT
-        "Store Location",
+        *,
         CASE
-            WHEN X IS NOT NULL AND X != '' AND X ~ '^-?[0-9]*\.?[0-9]+$' THEN CAST(X AS NUMERIC)
+            WHEN SUBSTRING(coordinates, 1, POSITION(', ' IN coordinates) - 1) ~ '^-?[0-9]*\.?[0-9]+$' THEN CAST(SUBSTRING(coordinates, 1, POSITION(', ' IN coordinates) - 1) AS NUMERIC(18,7))
             ELSE NULL
-        END AS X,
+        END AS y,
         CASE
-            WHEN Y IS NOT NULL AND Y != '' AND Y ~ '^-?[0-9]*\.?[0-9]+$' THEN CAST(Y AS NUMERIC)
+            WHEN SUBSTRING(coordinates, POSITION(', ' IN coordinates) + 2) ~ '^-?[0-9]*\.?[0-9]+$' THEN CAST(SUBSTRING(coordinates, POSITION(', ' IN coordinates) + 2) AS NUMERIC(18, 7))
             ELSE NULL
-        END AS Y
-    FROM x_y_extracted
-)
+        END AS x
+    FROM (
+        SELECT
+            *,
+            SUBSTRING(
+                "Store Location",
+                POSITION('(' IN "Store Location") + 1,
+                POSITION(')' IN "Store Location") - POSITION('(' IN "Store Location") - 1
+            ) AS coordinates
+        FROM liquor_sales
+    ) AS temp_coordinates
+) AS temp_xy_converted;
 
-UPDATE liquor_sales
+-- Step 3: Add New Columns to the liquor_sales Table
+ALTER TABLE points_geom DROP COLUMN x;
+ALTER TABLE points_geom DROP COLUMN y;
+ALTER TABLE points_geom DROP coordinates;
 
-SET X = x_y_converted.X,
-    Y = x_y_converted.Y
+SELECT DISTINCT * FROM points_geom LIMIT 10;
 
-FROM x_y_converted;
+SELECT DISTINCT COUNT(geom) from points_geom;
 
-SELECT ST_Point(X, Y) as points from liquor_sales;
+-- Drop rows where any column has a null value
+DELETE FROM points_geom
+WHERE geom IS NULL;
+
+SELECT COUNT(geom) from points_geom;
+
+SELECT DISTINCT ON (geom) * FROM points_geom LIMIT 10;
+
+COPY (SELECT DISTINCT ON (geom) * FROM points_geom) TO 'test03_duck.geojson'
+WITH (FORMAT GDAL, DRIVER 'GeoJSON');
